e6b47660790fede0909e97a2f62681b5c43736c1
Daniel Rodríguez Escobar <drodriguez@lessthanthree.tech>
2025-04-06 19:34:13 +0200

    Fixed tests
    Signed-off-by: Daniel Rodríguez Escobar <drodriguez@lessthanthree.tech>

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -4,11 +4,6 @@ import json
 import uuid
 import pytest
 
-def aws_credentials():
-    os.environ['AWS_ACCESS_KEY_ID'] = os.getenv('AWS_ACCESS_KEY_ID')
-    os.environ['AWS_SECRET_ACCESS_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY')
-    os.environ['AWS_REGION'] = os.getenv('AWS_REGION', 'eu-west-1')
-
 @pytest.fixture
 def setup_resources():   
     return {
